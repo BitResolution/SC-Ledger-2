abae7064e6a7d5170c868006a9e01ef481e4e8ad
James McGivern <bitresolution-github@sorrynoremorse.oib.com>
2013-05-29 20:36:03 +0100

    * Extending test coverage before completing vertical slice by adding persistence

---
+++ src/test/java/com/bitresolution/ledger/core/files/DefaultReportFileReaderFactoryTest.java
@@ -0,0 +1,40 @@
+package com.bitresolution.ledger.core.files;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+
+public class DefaultReportFileReaderFactoryTest {
+
+    @Test
+    public void shouldCreateInstancesOfReportFileReader() throws Exception {
+        //given
+        ReportFileReaderFactory factory = new DefaultReportFileReaderFactory();
+        File source = new File(this.getClass().getResource("/single-entry-example.txt").toURI());
+
+        //when
+        ReportFileReader reader = factory.getReader(source);
+
+        //then
+        assertThat(reader, is(not(nullValue())));
+        assertThat(reader, is(instanceOf(ReportFileReader.class)));
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void shouldProgagateExceptions() throws Exception {
+        //given
+        ReportFileReaderFactory factory = new DefaultReportFileReaderFactory();
+        File source = new File("file-that-should-never-exist");
+        assumeThat(source.exists(), is(false));
+
+        //when
+        factory.getReader(source);
+
+        //then expect error
+    }
+}
