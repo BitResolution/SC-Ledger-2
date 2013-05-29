226186e8c10d051e6a72a12126ae3c0208148d38
James McGivern <bitresolution-github@sorrynoremorse.oib.com>
2013-05-29 20:03:20 +0100

    * An attempt at cleaning up the ReportFileParser a little, will help with testing corner cases later

---
+++ src/main/java/com/bitresolution/ledger/core/files/LineType.java
@@ -0,0 +1,46 @@
+package com.bitresolution.ledger.core.files;
+
+import org.apache.commons.lang3.StringUtils;
+
+enum LineType {
+    PERIOD_OF_REPORT("CONFORMED PERIOD OF REPORT:"),
+    REPORT_FILING_DATE("FILED AS OF DATE:"),
+    ENTRY(""),
+    SKIPPABLE("");
+
+    private final String prefix;
+
+    LineType(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public int getPrefixLength() {
+        return prefix.length();
+    }
+
+    public static LineType of(String line) {
+        if(StringUtils.isBlank(line)
+                || line.startsWith(" ")
+                || line.startsWith("NAME OF ISSUER") //column header
+                || line.startsWith("<S>")
+                || line.startsWith("</TABLE>")
+                || line.startsWith("</TEXT>")
+                || line.startsWith("</DOCUMENT>")
+                || line.startsWith("</SEC-DOCUMENT>")) {
+            return SKIPPABLE;
+        }
+        else if(line.startsWith(PERIOD_OF_REPORT.getPrefix())) {
+            return PERIOD_OF_REPORT;
+        }
+        else if(line.startsWith(REPORT_FILING_DATE.getPrefix())) {
+            return REPORT_FILING_DATE;
+        }
+        else {
+            return ENTRY;
+        }
+    }
+}
--- src/main/java/com/bitresolution/ledger/core/files/ReportFileReader.java
+++ src/main/java/com/bitresolution/ledger/core/files/ReportFileReader.java
@@ -2,7 +2,6 @@ package com.bitresolution.ledger.core.files;
 
 import com.bitresolution.ledger.core.ledger.Entry;
 import com.bitresolution.ledger.core.ledger.Report;
-import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,26 +13,9 @@ public class ReportFileReader implements Readable {
 
     private static final Logger log = LoggerFactory.getLogger(ReportFileReader.class);
 
-    private static final String PERIOD_OF_REPORT = "CONFORMED PERIOD OF REPORT:";
-    private static final String FILING_DATE = "FILED AS OF DATE:";
-    private static final String COLUMN_HEADER = "NAME OF ISSUER";
-
-//    private static enum LineType {
-//        PERIOD_OF_REPORT,
-//        FILING_DATE,
-//        ENTRY,
-//        SKIPPABLE;
-//
-//        public static LineType of(String line) {
-//            return SKIPPABLE;
-//        }
-//    }
-
-    private final BufferedReader reader;
-    private final File source;
+    private final LineNumberReader reader;
 
     public ReportFileReader(File source) throws FileNotFoundException {
-        this.source = source;
         reader = new LineNumberReader(new FileReader(source));
     }
 
@@ -46,70 +28,64 @@ public class ReportFileReader implements Readable {
         Report.Builder report = new Report.Builder();
         String line;
         while((line = reader.readLine()) != null) {
-            if(StringUtils.isBlank(line)
-                    || line.startsWith(" ")
-                    || line.startsWith(COLUMN_HEADER)
-                    || line.startsWith("<S>")
-                    || line.startsWith("</TABLE>")
-                    || line.startsWith("</TEXT>")
-                    || line.startsWith("</DOCUMENT>")
-                    || line.startsWith("</SEC-DOCUMENT>")) {
-                log.trace("Skipping line: " + line);
-            }
-            else if(line.startsWith(PERIOD_OF_REPORT)) {
-                String value = line.substring(PERIOD_OF_REPORT.length()).trim();
-                DateTime periodOfReport = new DateTime(value);
-                report.setPeriodOfReport(periodOfReport);
-                log.debug("Parsed 'period of report' {} from file '{}'", periodOfReport, source.getName());
-            }
-            else if(line.startsWith(FILING_DATE)) {
-                String value = line.substring(FILING_DATE.length()).trim();
-                DateTime filingDate = new DateTime(value);
-                report.setFilingDate(filingDate);
-                log.debug("Parsed 'filing date' {} from file '{}'", filingDate, source.getName());
-            }
-            else {
-                try {
-                    String[] values = line.split("[\\s]{2,}");
-                    Entry.Builder entryBuilder = new Entry.Builder();
-                    entryBuilder.setNameOfIssuer(values[0]);
-                    entryBuilder.setTitleOfClass(values[1]);
-                    entryBuilder.setCusip(values[2]);
-                    entryBuilder.setMarketValue(values[3]);
-                    entryBuilder.setAmount(values[4]);
-                    entryBuilder.setCall(values[5]);
-                    entryBuilder.setInvestmentDescretion(values[6]);
-                    entryBuilder.setOtherManagers("");
-                    entryBuilder.setVotingAuthoritySole(values[7]);
-                    entryBuilder.setShared(values[8]);
-                    entryBuilder.setNone(values[9]);
-                    Entry entry = entryBuilder.build();
+            LineType lineType = LineType.of(line);
+            log.trace("line[{}] is type {}", new Object[]{reader.getLineNumber(), lineType.name()});
+            switch(lineType) {
+                case PERIOD_OF_REPORT:
+                    parsePeriodOfReport(line, lineType, report);
+                    break;
 
-                    report.addEntry(entry);
-                    log.debug("Parsed entry: {}", entry);
-                }
-                catch (ArrayIndexOutOfBoundsException e) {
-                    log.error("Error parsing entry line: {}", line);
-                }
+                case REPORT_FILING_DATE:
+                    parseFilingDate(line, lineType, report);
+                    break;
+
+                case ENTRY:
+                    parseEntry(line, lineType, report);
+                    break;
+
+                case SKIPPABLE:
+                    break;
             }
         }
         return report.build();
     }
-//    public Report readReport() throws IOException {
-//        Report report = new Report();
-//        String line;
-//        while((line = reader.readLine()) != null) {
-//            switch(LineType.of(line)) {
-//                case PERIOD_OF_REPORT:
-//                    break;
-//                case FILING_DATE:
-//                    break;
-//                case ENTRY:
-//                    break;
-//                case SKIPPABLE:
-//                    break;
-//            }
-//        }
-//        return report;
-//    }
+
+    private void parseEntry(String line, LineType type, Report.Builder report) {
+        try {
+            String[] values = line.split("[\\s]{2,}");
+            Entry.Builder entryBuilder = new Entry.Builder();
+            entryBuilder.setNameOfIssuer(values[0]);
+            entryBuilder.setTitleOfClass(values[1]);
+            entryBuilder.setCusip(values[2]);
+            entryBuilder.setMarketValue(values[3]);
+            entryBuilder.setAmount(values[4]);
+            entryBuilder.setCall(values[5]);
+            entryBuilder.setInvestmentDescretion(values[6]);
+            entryBuilder.setOtherManagers("");
+            entryBuilder.setVotingAuthoritySole(values[7]);
+            entryBuilder.setShared(values[8]);
+            entryBuilder.setNone(values[9]);
+            Entry entry = entryBuilder.build();
+
+            report.addEntry(entry);
+            log.debug("Line [{}] - parsed entry: {}", reader.getLineNumber(), entry);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            log.error("Error parsing entry line [{}]: {}", reader.getLineNumber(), line);
+        }
+    }
+
+    private void parseFilingDate(String line, LineType type, Report.Builder report) {
+        String value = line.substring(type.getPrefixLength()).trim();
+        DateTime filingDate = new DateTime(value);
+        report.setFilingDate(filingDate);
+        log.debug("Line [{}] - parsed 'filing date': {}", reader.getLineNumber(), filingDate);
+    }
+
+    private void parsePeriodOfReport(String line, LineType type, Report.Builder report) {
+        String value = line.substring(type.getPrefixLength()).trim();
+        DateTime periodOfReport = new DateTime(value);
+        report.setPeriodOfReport(periodOfReport);
+        log.debug("Line [{}] - parsed 'period of report': {}", reader.getLineNumber(), periodOfReport);
+    }
 }
