8dde64ee77c981c66f45948e89a12c9a7232bc11
James McGivern <bitresolution-github@sorrynoremorse.oib.com>
2013-06-02 18:16:11 +0100

    * Added lookup by period of report * and test for multiple report storage and listing

--- src/main/java/com/bitresolution/ledger/core/ledger/ReportRepository.java
+++ src/main/java/com/bitresolution/ledger/core/ledger/ReportRepository.java
@@ -1,6 +1,10 @@
 package com.bitresolution.ledger.core.ledger;
 
+import org.joda.time.DateTime;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends CrudRepository<Report, Long> {
+    List<Report> findByPeriodOfReport(DateTime period);
 }
--- src/test/java/com/bitresolution/ledger/core/ledger/ReportRepositorySpec.groovy
+++ src/test/java/com/bitresolution/ledger/core/ledger/ReportRepositorySpec.groovy
@@ -18,16 +18,46 @@ class ReportRepositorySpec extends Specification {
 
     def "should insert reports"() {
         given:
-        Report report = new Report()
-        report.periodOfReport = new DateTime(2013, 01, 04, 00, 00, 00, 00)
-        report.filingDate = new DateTime(2013, 04, 04, 0, 00, 00, 00)
+        Report report = new Report(null, new DateTime(2013, 1, 4, 00, 00, 00, 00), new DateTime(2013, 4, 4, 0, 00, 00, 00));
 
         when:
         Report result = repository.save(report)
 
         then:
         assert result.id != null
-        assert result.periodOfReport == new DateTime(2013, 01, 04, 00, 00, 00, 00)
-        assert result.filingDate == new DateTime(2013, 04, 04, 00, 00, 00, 00)
+        assert result.periodOfReport == new DateTime(2013, 1, 4, 00, 00, 00, 00)
+        assert result.filingDate == new DateTime(2013, 4, 4, 00, 00, 00, 00)
+    }
+
+    def "should find report by periodOfReport"() {
+        given:
+        DateTime periodOfReport = new DateTime(2013, 1, 4, 00, 00, 00, 00)
+        Report report = new Report(null, periodOfReport, new DateTime(2013, 4, 4, 0, 00, 00, 00));
+        repository.save(report)
+
+        when:
+        List<Report> reports = repository.findByPeriodOfReport(periodOfReport)
+
+        then:
+        assert reports.size() == 1
+        assert reports[0].id != null
+        assert reports[0].periodOfReport == new DateTime(2013, 1, 4, 00, 00, 00, 00)
+        assert reports[0].filingDate == new DateTime(2013, 4, 4, 00, 00, 00, 00)
+    }
+
+    def "should list all reports by periodOfReport"() {
+        given:
+        Report reportA = new Report(null, new DateTime(2013, 1, 4, 00, 00, 00, 00), new DateTime(2013, 4, 4, 0, 00, 00, 00))
+        Report reportB = new Report(null, new DateTime(2013, 5, 4, 00, 00, 00, 00), new DateTime(2013, 8, 4, 0, 00, 00, 00))
+        Report reportC = new Report(null, new DateTime(2013, 9, 4, 00, 00, 00, 00), new DateTime(2013, 12, 4, 0, 00, 00, 00))
+        repository.save(reportA)
+        repository.save(reportB)
+        repository.save(reportC)
+
+        when:
+        List<Report> reports = repository.findAll().toList()
+
+        then:
+        assert reports.size() == 3
     }
 }
